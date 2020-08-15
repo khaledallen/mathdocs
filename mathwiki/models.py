95679f9bd82da6ccabf24bfb1c69d109670886ab
Khaled Allen <khaled.allen@gmail.com>
2020-08-15 15:15:32 -0600

    mathjax and basic definitions

--- mathwiki/models.py
+++ mathwiki/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
 # Create your models here.
-class Definition(models.Model):
-    title = models.CharField(max_length = 200)
+class Axiom(models.Model):
+    title = models.CharField(max_length = 300)
     statement = models.TextField()
+
+class Definition(models.Model):
+    name = models.CharField(max_length = 300)
+    statement = models.TextField()
+    def __str__(self):
+        return self.name
     
 class Theorem(models.Model):
-    title = models.CharField(max_length = 200)
+    THEOREM_TYPE_CHOICES = (
+        ('L', 'Lemma'),
+        ('C', 'Corollary'),
+        ('P', 'Proposition'),
+        ('T', 'Theorem')
+    )
+    name = models.CharField(max_length = 300)
+    theorem_type = models.CharField(max_length = 1, choices = THEOREM_TYPE_CHOICES)
     proof = models.TextField()
     included_definitions = models.ManyToManyField(Definition)
+    def __str__(self):
+        return self.name
--- mathwiki/views.py
+++ mathwiki/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Definition
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the mathwiki index.")
+
+def definitiondetails(request, definition_id):
+    definition = Definition.objects.filter(id=definition_id)[0]
+    context = {'definition': definition}
+    return render(request, 'mathwiki/definitiondetails.html', context)
