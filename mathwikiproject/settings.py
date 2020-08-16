98798ee5852fbd65c1c87452af777f25ccd25550
Khaled Allen <khaled.allen@gmail.com>
2020-08-16 12:58:33 -0600

    better secrets

--- mathwikiproject/settings.py
+++ mathwikiproject/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#(t^rj2z%5!d%%gu%629&x-hlr%ue62ffmd0_vt8doe2krlixz'
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
